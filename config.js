de5fa6e74fd1163f6bed5b955e86cae52cbfe164
zhangzju <zhangway19921221@163.com>
2017-03-08 11:00:32 +0800

    add jenkins directory

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ self.$config = {
     //gitlab
     {title: 'Gitlab', path: '/gitlab/index'},
     //docekr
-    {title: 'Docker', path: '/docker/index'}
+    {title: 'Docker', path: '/docker/index'},
+    //jenkins
+    {title: 'Jenkins', path: '/jenkins/index'}
   ],
   sidebar: true
 }
